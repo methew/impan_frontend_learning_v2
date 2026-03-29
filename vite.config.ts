8b8495231deee4c00a421ddc9d35c4cb11d53618
methew <xiaoi2008@yeah.net>
2026-03-29 23:40:34 +0800

    fix: add missing index.html and main.tsx entry files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 const config = defineConfig({
   plugins: [
+    TanStackRouterVite(),
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart(),
     viteReact(),
   ],
+  server: {
+    port: 3003,
+    strictPort: true,
+  },
 })
 
 export default config
